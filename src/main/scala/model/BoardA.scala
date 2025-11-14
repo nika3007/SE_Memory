2ce1174950fdbaf4588d045932035cb92fa84139
nika3007 <skripnikveronika@icloud.com>
2025-11-14 08:31:59 +0100

    new boardspec

--- src/main/scala/model/BoardA.scala
+++ src/main/scala/model/BoardA.scala
@@ -30,8 +30,10 @@ final case class Board(cards: Vector[Card], selection: Option[Int] = None):
               selection = None // selection reinigen für andere karten
             )
           else
-          // beide Karten bleiben zunächst offen, kein sofortiges Umdrehen!
-          b2.copy(selection = None)
+          // beide Karten karten werden sofort umgedrehr, wenn kein match
+         b2.copy(
+            cards = b2.cards.updated(prev, c1.flip).updated(i, c2.flip),
+            selection = None
+          )
         (next, Some(isMatch))
-
   def allMatched: Boolean = cards.forall(_.isMatched)
